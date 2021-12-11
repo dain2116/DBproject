455ae00daaf6e49c67159149cf64bc5c179905b1
DESKTOP-80NQ4NL\user <dain2116@naver.com>
2021-12-11 22:04:46 +0900

    css

--- parking.js
+++ parking.js
@@ -1,9 +1,7 @@
-window.onload = function() {
-  const value = "search keyword";
+const value = "search keyword";
   for (let i = 0; i < videos.length; i++) {
     if (videos[i].snippet.title.indexOf(value) > -1) {
       console.log(videos[i].snippet.title);
     } else {
     }
-  }
 }
