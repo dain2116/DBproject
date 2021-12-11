93f1a9da9f0795bbf0cbf5f4123f8b89e264d809
DESKTOP-80NQ4NL\user <dain2116@naver.com>
2021-12-11 21:25:42 +0900

    parking, admin

---
+++ parking.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  const value = "search keyword";
+  for (let i = 0; i < videos.length; i++) {
+    if (videos[i].snippet.title.indexOf(value) > -1) {
+      console.log(videos[i].snippet.title);
+    } else {
+    }
+  }
+}
